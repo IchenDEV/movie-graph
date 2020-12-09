a27f352264a89e2f00454c41484ffaebab623341
idevlab <idevlab@outlook.com>
2020-12-09 18:06:53 +0800

    fix bugs

---
+++ src/views/NodeView.vue
@@ -0,0 +1,105 @@
+<template>
+<v-col style="padding:1rem" justify="center" align="center">
+<v-row class="mb-4">
+    <v-card class="d-inline-block mx-auto" style="padding:1rem" rounded="xl">
+    <v-container>
+      <v-row justify="space-around" >
+        <v-col cols="2" justify="center" align="center">
+          <v-img
+            height="200"
+            width="200"
+            src="https://tse2-mm.cn.bing.net/th/id/OIP.NAQub_yRSIzsetBq8y2RHwHaFj?pid=Api&rs=1"
+          ></v-img>
+        </v-col>
+
+        <v-col
+          cols="5"
+          class="text-center pl-0"
+        >
+          <v-row
+            class="flex-column ma-0 fill-height"
+            justify="center"
+          >
+            <v-col class="px-0">
+             功夫机器侠
+            </v-col>
+
+            <v-col class="px-0">
+              Vuetify 是一个用于 Vue.js 的 Material Design 组件框架。它的目标是提供所有必要的工具来创建美丽的内容丰富的应用程序。语义化的 Material 组件 已为你需要在项目中
+            </v-col>
+
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+
+               <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+
+               <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+  
+  
+  
+  </v-row>
+  <v-row justify="center" align="center">
+     <v-card class="d-inline-block mx-auto" style="padding:1rem" rounded="xl">
+    <v-container>
+      <v-row justify="space-around" >
+        <v-col cols="2" justify="center" align="center">
+          <v-img
+            height="200"
+            width="200"
+            src="https://tse2-mm.cn.bing.net/th/id/OIP.NAQub_yRSIzsetBq8y2RHwHaFj?pid=Api&rs=1"
+          ></v-img>
+        </v-col>
+
+        <v-col
+          cols="5"
+          class="text-center pl-0"
+        >
+          <v-row
+            class="flex-column ma-0 fill-height"
+            justify="center"
+          >
+            <v-col class="px-0">
+             功夫机器侠
+            </v-col>
+
+            <v-col class="px-0">
+              Vuetify 是一个用于 Vue.js 的 Material Design 组件框架。它的目标是提供所有必要的工具来创建美丽的内容丰富的应用程序。语义化的 Material 组件 已为你需要在项目中
+            </v-col>
+
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+
+               <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+
+               <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+  
+  
+  
+  </v-row>
+</v-col>
+  
+</template>
